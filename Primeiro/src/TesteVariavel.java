f6d7fb573227760959193fdc564e64333f1e34d3
ELAINE D ADDERIO FRAGA <elaine.dafraga@senacsp.edu.br>
2023-01-21 10:22:28 -0300

    atualizacao

---
+++ Primeiro/src/TesteVariavel.java
@@ -0,0 +1,15 @@
+
+public class TesteVariavel {
+
+	public static void main(String[] args) {
+		String mensagem = "Segundo hello";
+		//System.out.println(mensagem);
+		mensagem = "mudei";
+		System.out.println(mensagem);
+
+		
+	}
+
+}
+//String não é uma variável tipo primitivo. String é uma variável que guarda texto
+		// "=" significa atribuição
