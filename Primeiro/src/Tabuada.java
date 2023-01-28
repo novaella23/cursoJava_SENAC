90d8e1e1d4331ed940c7b108fa9419f463837389
novaella23 <dadderio79@outlook.com>
2023-01-28 12:05:13 -0300

    atualiza

---
+++ Primeiro/src/Tabuada.java
@@ -0,0 +1,20 @@
+
+public class Tabuada {
+	public static void main(String[] args) {
+		
+		
+		int contador=1;
+		int num1 = 1;
+				
+				while(contador <=10) {
+					int multiplica = num1 * contador;
+					System.out.println(num1 + "x" + contador + "=" + multiplica);
+					contador++;
+				}
+								
+			
+				while(num1 <=10) {
+					num1++;
+				}
+					
+				}
