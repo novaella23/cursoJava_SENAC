e413851ac8d631c9049ec90f8d6e0572567bddf3
ELAINE D ADDERIO FRAGA <elaine.dafraga@senacsp.edu.br>
2023-01-28 08:35:31 -0300

    atualiza

--- Primeiro/src/TesteTipoVariavel.java
+++ Primeiro/src/TesteTipoVariavel.java
@@ -3,11 +3,20 @@ public class TesteTipoVariavel {
 
 	public static void main(String[] args) {
 		int valor1 = 10;
+		
 		int valor2 = 6;
+		
 		int soma;
+		
+		String mensagem = "";
+		
+		mensagem = valor1 + "+" + valor2;
+		
 		soma = valor1 + valor2;
 		
-		System.out.println(valor1 + " + " + valor2 + " = " + soma);
+		mensagem += "=" + soma;
+		
+		System.out.println(mensagem);
 		
 		
 	}
